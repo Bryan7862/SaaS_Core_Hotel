b3e4eb1853c9d6bdfb0f46bb6d81277eaea789e3
Sieghart Saladin <hisokamorow786@gmail.com>
2025-12-21 10:26:56 -0500

    fix(frontend):se resolvio los conflictos de enrutamiento e integrar páginas modulares

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,12 +6,12 @@ import { UsersPage } from './modules/iam/pages/UsersPage'; // Modular Version
 import { OrganizationsPage } from './modules/organizations/pages/OrganizationsPage';
 import { RegisterPage } from './pages/RegisterPage';
 import { LoginPage } from './modules/auth/pages/LoginPage';
-<<<<<<< HEAD
+import { TrashPage } from './modules/trash/pages/TrashPage';
 import { ProfilePage } from './pages/ProfilePage';
 import { SettingsPage } from './pages/SettingsPage';
 import { BillingPage } from './pages/BillingPage';
 import { CreateInvoicePage } from './pages/CreateInvoicePage';
-
+import { RequireAuth } from './components/RequireAuth';
 import { ThemeProvider } from './context/ThemeContext';
 
 function App() {
@@ -24,53 +24,29 @@ function App() {
                     <Route
                         path="/*"
                         element={
-=======
-import { TrashPage } from './modules/trash/pages/TrashPage';
+                            <RequireAuth>
+                                <Layout>
+                                    <Routes>
+                                        <Route path="/" element={<DashboardPage />} />
+                                        {/* <Route path="/roles" element={<RolesPage />} /> */}
+                                        <Route path="/users" element={<UsersPage />} />
+                                        <Route path="/organizations" element={<OrganizationsPage />} />
+                                        <Route path="/trash" element={<TrashPage />} />
 
-import { RequireAuth } from './components/RequireAuth';
-
-function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/login" element={<LoginPage />} />
-                <Route path="/register" element={<RegisterPage />} />
-                <Route
-                    path="/*"
-                    element={
-                        <RequireAuth>
->>>>>>> origin/master
-                            <Layout>
-                                <Routes>
-                                    <Route path="/" element={<DashboardPage />} />
-                                    {/* <Route path="/roles" element={<RolesPage />} /> */}
-                                    <Route path="/users" element={<UsersPage />} />
-                                    <Route path="/organizations" element={<OrganizationsPage />} />
-<<<<<<< HEAD
-
-                                    {/* New Functional Routes */}
-                                    <Route path="/profile" element={<ProfilePage />} />
-                                    <Route path="/settings/general" element={<SettingsPage />} />
-                                    <Route path="/settings/billing" element={<BillingPage />} />
-                                    <Route path="/settings/orgs" element={<OrganizationsPage />} />
-                                    <Route path="/invoices/new" element={<CreateInvoicePage />} />
-                                </Routes>
-                            </Layout>
+                                        {/* New Functional Routes */}
+                                        <Route path="/profile" element={<ProfilePage />} />
+                                        <Route path="/settings/general" element={<SettingsPage />} />
+                                        <Route path="/settings/billing" element={<BillingPage />} />
+                                        <Route path="/settings/orgs" element={<OrganizationsPage />} />
+                                        <Route path="/invoices/new" element={<CreateInvoicePage />} />
+                                    </Routes>
+                                </Layout>
+                            </RequireAuth>
                         }
                     />
                 </Routes>
             </BrowserRouter>
         </ThemeProvider>
-=======
-                                    <Route path="/trash" element={<TrashPage />} />
-                                </Routes>
-                            </Layout>
-                        </RequireAuth>
-                    }
-                />
-            </Routes>
-        </BrowserRouter>
->>>>>>> origin/master
     );
 }
 
