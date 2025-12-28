9e5deca8cbbb35719e7c8dc363f54a670568d800
Sieghart Saladin <hisokamorow786@gmail.com>
2025-12-27 20:31:58 -0500

    tarea(auditoría):para exportar el servicio de transacciones para usarlo en el módulo de pagos

--- backend/src/modules/transactions/transactions.module.ts
+++ backend/src/modules/transactions/transactions.module.ts
@@ -13,5 +13,6 @@ import { AuthModule } from '../auth/auth.module';
     ],
     controllers: [TransactionsController],
     providers: [TransactionsService],
+    exports: [TransactionsService],
 })
 export class TransactionsModule { }
